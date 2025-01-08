f31766b3fe7775a6ed188217030512ae2f283219
ufukesra <ufukesra3458@gmail.com>
2025-01-08 00:19:32 +0000

    Test group performed with the befaoreEach and afterEach

--- playwright.config.js
+++ playwright.config.js
@@ -15,7 +15,7 @@ module.exports = defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : 1,
   reporter: 'html',
   use: {
     trace: 'on-first-retry',
